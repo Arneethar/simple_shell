2d1f064b7017aab5d09ef86247801b33e3338dac
Arneethar <anitamatthewc1@gmail.com>
2021-11-01 03:54:20 -0700

    test work

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 /**
  * _putchar - function that outputs a character
  * @c: input character
@@ -59,4 +59,99 @@ while (str[i] != '\0')
 _putchar(str[i]);
 i++;
 }
+}
+/**
+ * _strcmp - Compare two strings
+ * @s1: string
+ * @s2: string
+ * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+/**
+ *_strcpy - prints n elements of an array of integers
+ *@dest: input
+ *@src: input
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *r = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (r);
+}
+/**
+ * read_cmd reading user input
+ * return: ptr
+ */
+
+char *read_cmd(void)
+{
+    char buf[1024];
+    char *ptr = NULL;
+    char ptrlen = 0;
+
+    while(fgets(buf, 1024, stdin))
+    {
+        int buflen = _strlen(buf);
+
+        if(!ptr)
+        {
+            ptr = malloc(buflen+1);
+        }
+        else
+        {
+            char *ptr2 = realloc(ptr, ptrlen+buflen+1);
+
+            if(ptr2)
+            {
+                ptr = ptr2;
+            }
+            else
+            {
+                free(ptr);
+                ptr = NULL;
+            }
+        }
+
+        if(!ptr)
+        {
+            fprintf(stderr, "failed to alloc buffer: %s\n", strerror(errno));
+            return NULL;
+        }
+
+        _strcpy(ptr+ptrlen, buf);
+
+        if(buf[buflen-1] == '\n')
+        {
+            if(buflen == 1 || buf[buflen-2] != '\\')
+            {
+                return ptr;
+            }
+
+            ptr[ptrlen+buflen-2] = '\0';
+            buflen -= 2;
+            print_prompt2();
+        }
+
+        ptrlen += buflen;
+    }
+
+    return ptr;
 }
